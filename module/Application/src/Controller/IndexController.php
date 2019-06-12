bb7f91a15361729b759bb1a1cb2881ec06dd4dc2
comcduarte <33262448+comcduarte@users.noreply.github.com>
2019-06-12 13:53:20 -0400

    Display Sectional Hyperlinks

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -7,11 +7,16 @@
 
 namespace Application\Controller;
 
+use Application\Model\HyperlinkModel;
+use Midnet\Exception\Exception;
+use Zend\Db\Adapter\AdapterAwareTrait;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Sql\Join;
+use Zend\Db\Sql\Select;
+use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Where;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-use Zend\Db\Adapter\AdapterAwareTrait;
-use Application\Model\HyperlinkModel;
-use Zend\Db\Sql\Where;
 
 class IndexController extends AbstractActionController
 {
@@ -23,12 +28,36 @@ class IndexController extends AbstractActionController
         $this->layout('layout/metromega');
         
         $model = new HyperlinkModel($this->adapter);
+       
+        /*******************************/
         
+        $sql = new Sql($this->adapter);
+        
+        $select = new Select();
+        $select->from('section_links')
+            ->join('links', 'section_links.LINK = links.UUID', ['*'], Join::JOIN_INNER)
+            ->join('sections', 'section_links.SECTION = sections.UUID', ['UUID_S'=>'UUID', 'Name' => 'NAME'], Join::JOIN_INNER);
         $predicate = new Where();
-        $predicate->equalTo('STATUS', $model::ACTIVE_STATUS);
-        $data = $model->fetchAll($predicate);
-        $view->setVariable('data', $data);
+        $predicate->equalTo('links.STATUS', $model::ACTIVE_STATUS);
+        $select->where($predicate);
         
+        $statement = $sql->prepareStatementForSqlObject($select);
+        $resultSet = new ResultSet();
+        try {
+            $results = $statement->execute();
+            $resultSet->initialize($results);
+        } catch (Exception $e) {
+            return $e;
+        }
+        
+        $data = [];
+        
+        foreach ($resultSet->toArray() as $record) {
+            $data[$record['Name']][] = $record;
+        }
+        
+        
+        $view->setVariable('data', $data);
         return ($view);
     }
 }
